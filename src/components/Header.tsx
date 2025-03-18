d479b52293640cd380823d15893b5a45822d408b
Jon Perry <jonperry113@gmail.com>
2025-03-18 17:26:15 -0400

    Built logoutbutton

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,9 +3,10 @@ import Image from "next/image";
 import Link from "next/link";
 import { Button } from "./ui/button";
 import DarkModeToggle from "./DarkModeToggle";
+import LogOutButton from "./LogOutButton";
 
 function Header() {
-  const user = null;
+  const user = 1;
   return (
     <header
       className="bg-popover relative flex h-24 w-full items-center justify-between px-3 sm:px-8"
@@ -28,7 +29,7 @@ function Header() {
 
       <div className="flex gap-4">
         {user ? (
-          "Logout"
+          <LogOutButton />
         ) : (
           <>
             <Button asChild className="hidden sm:block">
