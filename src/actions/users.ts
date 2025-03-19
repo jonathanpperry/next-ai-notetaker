28f32c6e7a8286d8a3274410ddcf8f4761d0dbe0
Jon Perry <jonperry113@gmail.com>
2025-03-19 09:55:47 -0400

    Add prisma code

--- src/actions/users.ts
+++ src/actions/users.ts
@@ -1,6 +1,7 @@
 "use server"
 
 import { createClient } from "@/auth/server"
+import { prisma } from "@/db/prisma"
 import { handleError } from "@/lib/utils"
 
 export const loginAction = async (email: string, password: string) => {
@@ -44,6 +45,12 @@ export const signUpAction = async (email: string, password: string) => {
         if (!userId) throw new Error("Error signing up");
 
         // Add user to database
+        await prisma.user.create({
+            data: {
+                id: userId,
+                email
+            }
+        });
 
         return { errorMessage: null }
     } catch (error) {
